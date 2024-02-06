10bd80dbb79e103c5f756056d85b5288b3ea27a0
Nick Sullivan <nick.dave.sullivan@gmail.com>
2024-02-07 06:12:49 +1030

    triggering cloudfront invalidation every time

--- terraform/upload/main.tf
+++ terraform/upload/main.tf
@@ -48,6 +48,7 @@ resource "aws_s3_object" "static_files" {
 
 resource "terraform_data" "clear_cloudfront_cache" {
   depends_on = [aws_s3_object.static_files]
+  triggers_replace = [timestamp()]  # Triggers every time
 
   provisioner "local-exec" {
     command     = "aws cloudfront create-invalidation --distribution-id ${data.aws_ssm_parameter.cloudfront_distribution_id.value} --paths '/*'"
