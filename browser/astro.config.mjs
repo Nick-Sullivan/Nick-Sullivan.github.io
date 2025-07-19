cae33f3028a0db5e5ffe7a3ccf2e22182e0fd40c
Nick Sullivan <nick.dave.sullivan@gmail.com>
2025-07-19 16:34:19 +0930

    adding umami

--- browser/astro.config.mjs
+++ browser/astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import umami from "@yeskunall/astro-umami";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,7 +10,11 @@ export default defineConfig({
     prefetchAll: true,
     defaultStrategy: "viewport",
   },
-  integrations: [mdx(), react()],
+  integrations: [
+    mdx(),
+    react(),
+    umami({ id: "8b3578ce-5dd5-4c43-8f86-2a6e8adcf606" }),
+  ],
   markdown: {
     syntaxHighlight: "shiki",
   },
