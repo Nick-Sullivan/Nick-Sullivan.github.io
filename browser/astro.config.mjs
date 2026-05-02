106549d7585ea31a38108b9ed2302713983df5c8
Nick Sullivan <nick.dave.sullivan@gmail.com>
2026-05-02 15:45:16 +0930

    better font loading

--- browser/astro.config.mjs
+++ browser/astro.config.mjs
@@ -1,11 +1,25 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import critters from "astro-critters";
 
 // https://astro.build/config
 export default defineConfig({
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: "PT Serif",
+      cssVariable: "--font-pt-serif",
+      display: "block",
+    },
+    {
+      provider: fontProviders.google(),
+      name: "Caveat",
+      cssVariable: "--font-caveat",
+      display: "block",
+    },
+  ],
   prefetch: {
     prefetchAll: true,
     defaultStrategy: "hover",
